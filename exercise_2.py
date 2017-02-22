004b2fa2e21b6c6130641a24812c9370b5f66387
Damian Szymanski <damian.szymanski22@gmail.com>
2017-02-22 23:59:19 +0100

    Rozwiązanie

--- exercise_2.py
+++ exercise_2.py
@@ -1,11 +1,18 @@
 # Zadanie 2
 
-def football_win(Ag, Bw, Aw, Bg):
-    if (Ag + Aw > Bw + Bg) or (Ag + Aw == Bw + Bg and Aw > Bw):
-        return "1"
-    elif (Ag + Aw < Bw + Bg) or (Ag + Aw == Bw + Bg and Aw < Bw):
-        return "2"
-    else:
-        return "x"
+def football_win(a_home, b_away, a_away, b_home):
+    if a_home + a_away > b_home + b_away:
+        return '1'
+    elif a_home + a_away < b_home + b_away:
+        return '2'
+    elif a_home + a_away == b_home + b_away:
+        if a_away > b_away:
+            return '1'
+        elif a_away < b_away:
+            return '2'
+        else:
+            return 'x'
 
-print(football_win(0, 3, 4, 1))
+print(football_win(2, 0, 0, 2))
+print(football_win(2, 1, 2, 3))
+print(football_win(0, 1, 0, 1))
