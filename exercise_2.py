789b8e7ea28129eb2635bbf128fbf3aa52e68cdc
Damian Szymanski <damian.szymanski22@gmail.com>
2017-02-22 23:57:25 +0100

    Rozwiązanie

--- exercise_2.py
+++ exercise_2.py
@@ -1,1 +1,11 @@
 # Zadanie 2
+
+def football_win(Ag, Bw, Aw, Bg):
+    if (Ag + Aw > Bw + Bg) or (Ag + Aw == Bw + Bg and Aw > Bw):
+        return "1"
+    elif (Ag + Aw < Bw + Bg) or (Ag + Aw == Bw + Bg and Aw < Bw):
+        return "2"
+    else:
+        return "x"
+
+print(football_win(0, 3, 4, 1))
