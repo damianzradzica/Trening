cc7b47ba4470e7198cc7c4d7f9297c1fefcb4162
Damian Szymanski <damian.szymanski22@gmail.com>
2017-02-27 17:18:55 +0100

    Rozwiązanie części b  zad. 6

--- exercise_6.py
+++ exercise_6.py
@@ -4,6 +4,17 @@ def safe_get(list, index):
     except IndexError:
         return None
 
-print(safe_get([1, 2, 3, 4, 5], 2))
-print(safe_get([1, 2, 3, 4, 5], 7))
+# print(safe_get([1, 2, 3, 4, 5], 2))
+# print(safe_get([1, 2, 3, 4, 5], 7))
 
+def divide(a, b):
+    if isinstance(a, int) and isinstance(b, int) and a >= 0 and b > 0:
+        return a / b
+    elif b == 0:
+        raise ValueError('Wartość parametru b musi być większa od zera')
+    else:
+        raise TypeError('Argumenty funkcji muszą być liczbami całkowitymi')
+
+# print(divide(4, 2))
+# # print(divide(4, 'osiem'))
+# print(divide(4, 0))
