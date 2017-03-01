f4533791466bca101ea3f98e0c5bb8c93059bb08
Damian Szymanski <damian.szymanski22@gmail.com>
2017-03-01 23:18:44 +0100

    Dodanie cześci b dla zadania 9

--- exercise_9.py
+++ exercise_9.py
@@ -1,6 +1,6 @@
 next_id = 0
 class Product(object):
-    def __init__(self, id, name, description, price, quantity):
+    def __init__(self, name, description, price, quantity):
         global next_id
         self.id = next_id
         next_id += 1
@@ -24,4 +24,24 @@ class Product(object):
     def get_total_sum(self):
         result = self.quantity * self.price
         return result
+
+class ShoppingCart(object):
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, new_product):
+        self.products.append(new_product)
+
+    def remove_product(self, product_id):
+        for prod in self.products:
+            if prod.id == product_id:
+                self.products.remove(prod)
+
+    def change_product_quantity(self, product_id, new_quantity):
+        for prod in self.products:
+            if prod.id == product_id:
+                self.products.quantity = new_quantity
+
+
+
 
